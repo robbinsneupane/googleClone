f98ed52074af4e1ce536f78b78d5333c8b04b03e
Rabins <rabinsneupane07@gmail.com>
2023-02-11 15:41:01 +0530

    worked on header

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 import { Inter } from "@next/font/google";
+import Header from "Components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
